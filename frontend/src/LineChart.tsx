53f8ebb4199c8fe57e1da161afab347f5a7d4cf9
ha-da-ra <gold85600@gmail.com>
2023-06-06 11:31:15 +0200

    set colors for linechart

--- frontend/src/LineChart.tsx
+++ frontend/src/LineChart.tsx
@@ -4,9 +4,7 @@ import {
   LinearScale,
   PointElement,
   LineElement,
-  Title,
-  Tooltip,
-  Legend,
+  Tooltip
 } from 'chart.js';
 import { Line } from 'react-chartjs-2';
 
@@ -15,35 +13,50 @@ ChartJS.register(
   LinearScale,
   PointElement,
   LineElement,
-  Title,
-  Tooltip,
-  Legend
+  Tooltip
 );
 
 export const options = {
     responsive: true,
     maintainAspectRatio: false,
-    plugins: {
-      legend: {
-        position: 'top' as const,
-      },
-      title: {
-        display: true,
-        text: 'Chart.js Line Chart',
-      },
-    },
+    scales: {
+      y: {
+        offset:true,
+        min: -2,
+        max: 2,
+        ticks: {
+          stepSize: 1
+        }
+      }
+    }
   };
 
 const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
-
+const moodData = [0,1,2,0,-1,-1,-2];
+const moodColors = moodData.map(mood => {
+  switch(mood) {
+    case -2:
+      return "rgba(194, 70, 100, 0.5)"
+    case -1:
+      return "rgba(129, 70, 194, 0.5)"
+    case 0:
+      return "rgba(70, 155, 194, 0.5)"
+    case 1: 
+    return "rgba(70, 194, 158, 0.5)"
+    case 2:
+      return "rgba(70, 194, 78, 0.5)"
+  }});
 export const data = {
   labels,
   datasets: [
     {
       label: 'Dataset 1',
-      data: [0,1,2,0,-1,-1,-2],
-      borderColor: 'rgb(255, 99, 132)',
-      backgroundColor: 'rgba(255, 99, 132, 0.5)',
+      data: moodData,
+      borderColor: "rgb(199, 199, 199)",
+      pointBorderColor: moodColors,
+      pointBackgroundColor: moodColors,
+      pointStyle: 'circle',
+      pointRadius: 10
     }
   ],
 };
